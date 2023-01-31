d37eac0d5ab7a6b0e3661df290f51125a86abffd
Adam Grare <adam@grare.com>
2023-01-31 11:06:21 -0500

    Add specs for Choice state

--- spec/workflow/states/choice_spec.rb
+++ spec/workflow/states/choice_spec.rb
@@ -1,6 +1,33 @@
 RSpec.describe ManageIQ::Floe::Workflow::States::Choice do
-  let(:workflow) { ManageIQ::Floe::Workflow.load(GEM_ROOT.join("examples/workflow.json")) }
+  let(:workflow) { ManageIQ::Floe::Workflow.load(GEM_ROOT.join("examples/workflow.json"), inputs) }
   let(:state)    { workflow.states_by_name["ChoiceState"] }
+  let(:inputs)   { {} }
+
+  describe "#run!" do
+    context "with a missing variable" do
+      it "raises an exception" do
+        expect { state.run! }.to raise_error(RuntimeError, "No such variable [$.foo]")
+      end
+    end
+
+    context "with an input value matching a condition" do
+      let(:inputs) { {"foo" => 1} }
+
+      it "returns the next state" do
+        next_state, = state.run!
+        expect(next_state).to eq(workflow.states_by_name["FirstMatchState"])
+      end
+    end
+
+    context "with an input value not matching any condition" do
+      let(:inputs) { {"foo" => 4} }
+
+      it "returns the default state" do
+        next_state, = state.run!
+        expect(next_state).to eq(workflow.states_by_name["FailState"])
+      end
+    end
+  end
 
   it "#to_dot" do
     expect(state.to_dot).to eq "  ChoiceState [ shape=diamond ]"
