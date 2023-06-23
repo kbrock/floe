b9846455f9a8cee873fc420a7f3e81f56372cd12
Adam Grare <adam@grare.com>
2023-06-23 13:30:01 -0400

    Docker async runner

--- lib/floe/workflow/runner/docker.rb
+++ lib/floe/workflow/runner/docker.rb
@@ -43,9 +43,70 @@ module Floe
           secrets_file&.close!
         end
 
+        def run_async!(resource, env = {}, secrets = {})
+          raise ArgumentError, "Invalid resource" unless resource&.start_with?("docker://")
+
+          image = resource.sub("docker://", "")
+
+          params = ["run", :detach]
+          params += [[:net, "host"]] if network == "host"
+          params += env.map { |k, v| [:e, "#{k}=#{v}"] } if env
+
+          secrets_file = nil
+
+          if secrets && !secrets.empty?
+            secrets_file = Tempfile.new
+            secrets_file.write(secrets.to_json)
+            secrets_file.flush
+
+            params << [:e, "SECRETS=/run/secrets"]
+            params << [:v, "#{secrets_file.path}:/run/secrets:z"]
+          end
+
+          params << image
+
+          logger.debug("Running docker: #{AwesomeSpawn.build_command_line("docker", params)}")
+
+          begin
+            container_id = docker!(*params).output
+          rescue
+            secrets_file&.close!
+            raise
+          end
+
+          [container_id, secrets_file]
+        end
+
+        def cleanup(container_id, secrets_file)
+          delete_container(container_id)
+          secrets_file&.close!
+        end
+
+        def running?(container_id)
+          JSON.parse(docker!("inspect", container_id).output).first.dig("State", "Running")
+        end
+
+        def success?(container_id)
+          JSON.parse(docker!("inspect", container_id).output).first.dig("State", "ExitCode") == 0
+        end
+
+        def output(container_id)
+          docker!("logs", container_id).output
+        end
+
         private
 
         attr_reader :network
+
+        def delete_container(container_id)
+          docker!("rm", container_id)
+        rescue
+          nil
+        end
+
+        def docker!(*params, **kwargs)
+          AwesomeSpawn.run!("docker", :params => params, **kwargs)
+        end
       end
     end
   end
--- spec/workflow/runner/docker_spec.rb
+++ spec/workflow/runner/docker_spec.rb
@@ -1,39 +1,101 @@
 RSpec.describe Floe::Workflow::Runner::Docker do
+  require "securerandom"
+
   let(:subject)        { described_class.new(runner_options) }
   let(:runner_options) { {} }
 
-  describe "#run!" do
+  let(:subject)      { described_class.new }
+  let(:container_id) { SecureRandom.hex }
+
+  describe "#run_async!" do
     it "raises an exception without a resource" do
-      expect { subject.run!(nil) }.to raise_error(ArgumentError, "Invalid resource")
+      expect { subject.run_async!(nil) }.to raise_error(ArgumentError, "Invalid resource")
     end
 
     it "raises an exception for an invalid resource uri" do
-      expect { subject.run!("arn:abcd:efgh") }.to raise_error(ArgumentError, "Invalid resource")
+      expect { subject.run_async!("arn:abcd:efgh") }.to raise_error(ArgumentError, "Invalid resource")
     end
 
     it "calls docker run with the image name" do
-      stub_good_run!("docker", :params => ["run", :rm, "hello-world:latest"])
+      stub_good_run!("docker", :params => ["run", :detach, "hello-world:latest"], :output => container_id)
 
-      subject.run!("docker://hello-world:latest")
+      subject.run_async!("docker://hello-world:latest")
     end
 
     it "passes environment variables to docker run" do
-      stub_good_run!("docker", :params => ["run", :rm, [:e, "FOO=BAR"], "hello-world:latest"])
+      stub_good_run!("docker", :params => ["run", :detach, [:e, "FOO=BAR"], "hello-world:latest"], :output => container_id)
 
-      subject.run!("docker://hello-world:latest", {"FOO" => "BAR"})
+      subject.run_async!("docker://hello-world:latest", {"FOO" => "BAR"})
     end
 
     it "passes a secrets volume to docker run" do
-      stub_good_run!("docker", :params => ["run", :rm, [:e, "FOO=BAR"], [:e, "_CREDENTIALS=/run/secrets"], [:v, a_string_including(":/run/secrets")], "hello-world:latest"])
+      stub_good_run!("docker", :params => ["run", :detach, [:e, "FOO=BAR"], [:e, "SECRETS=/run/secrets"], [:v, a_string_including(":/run/secrets")], "hello-world:latest"], :output => container_id)
 
-      subject.run!("docker://hello-world:latest", {"FOO" => "BAR"}, {"luggage_password" => "12345"})
+      subject.run_async!("docker://hello-world:latest", {"FOO" => "BAR"}, {"luggage_password" => "12345"})
+    end
+  end
+
+  describe "#running?" do
+    it "returns true when running" do
+      stub_good_run!("docker", :params => ["inspect", container_id], :output => "[{\"State\": {\"Running\": true}}]")
+      expect(subject.running?(container_id)).to be_truthy
+    end
+
+    it "returns false when completed" do
+      stub_good_run!("docker", :params => ["inspect", container_id], :output => "[{\"State\": {\"Running\": false, \"ExitCode\": 0}}]")
+      expect(subject.running?(container_id)).to be_falsey
+    end
+  end
+
+  describe "#success?" do
+    it "returns true when successful" do
+      stub_good_run!("docker", :params => ["inspect", container_id], :output => "[{\"State\": {\"Running\": false, \"ExitCode\": 0}}]")
+      expect(subject.success?(container_id)).to be_truthy
+    end
+
+    it "returns false when unsuccessful" do
+      stub_good_run!("docker", :params => ["inspect", container_id], :output => "[{\"State\": {\"Running\": false, \"ExitCode\": 1}}]")
+      expect(subject.success?(container_id)).to be_falsey
+    end
+  end
+
+  describe "#output" do
+    it "returns log output" do
+      stub_good_run!("docker", :params => ["logs", container_id], :output => "hello, world!")
+      expect(subject.output(container_id)).to eq("hello, world!")
+    end
+
+    it "raises an exception when getting pod logs fails" do
+      stub_bad_run!("docker", :params => ["logs", container_id])
+      expect { subject.output(container_id) }.to raise_error(AwesomeSpawn::CommandResultError, /docker exit code: 1/)
+    end
+  end
+
+  describe "#cleanup" do
+    let(:secrets_file) { double("Tempfile") }
+
+    it "deletes the container and secret" do
+      stub_good_run!("docker", :params => ["rm", container_id])
+      expect(secrets_file).to receive(:close!)
+      subject.cleanup(container_id, secrets_file)
+    end
+
+    it "doesn't delete the secret_file if not passed" do
+      stub_good_run!("docker", :params => ["rm", container_id])
+      subject.cleanup(container_id, nil)
+    end
+
+    it "deletes the secrets file if deleting the container fails" do
+      stub_bad_run!("docker", :params => ["rm", container_id])
+      expect(secrets_file).to receive(:close!)
+      subject.cleanup(container_id, secrets_file)
     end
 
     context "with network=host" do
       let(:runner_options) { {"network" => "host"} }
 
       it "calls docker run with --net host" do
-        stub_good_run!("docker", :params => ["run", :rm, [:net, "host"], "hello-world:latest"])
+        stub_good_run!("docker", :params => ["run", :rm, "hello-world:latest"])
 
         subject.run!("docker://hello-world:latest")
       end
