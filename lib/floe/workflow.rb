b34e6e9ecd030a2188a26e27553b2ed4e9567c18
Keenan Brock <keenan@thebrocks.net>
2023-08-04 22:56:33 -0400

    save @start_at in workflow
    we are saving all parameters in the state.
    
    Workflow#start_at instance variable
    
    this way we are not dipping into payload at runtime

--- lib/floe/workflow.rb
+++ lib/floe/workflow.rb
@@ -14,7 +14,7 @@ module Floe
       end
     end
 
-    attr_reader :context, :credentials, :output, :payload, :states, :states_by_name, :current_state, :status
+    attr_reader :context, :credentials, :output, :payload, :states, :states_by_name, :current_state, :status, :start_at
 
     def initialize(payload, context = nil, credentials = {})
       payload     = JSON.parse(payload)     if payload.kind_of?(String)
@@ -24,10 +24,10 @@ module Floe
       @payload     = payload
       @context     = context
       @credentials = credentials
+      @start_at    = payload["StartAt"]
 
       @states         = payload["States"].to_a.map { |name, state| State.build!(self, name, state) }
       @states_by_name = @states.each_with_object({}) { |state, result| result[state.name] = state }
-      start_at        = @payload["StartAt"]
 
       context.state["Name"] ||= start_at
 
