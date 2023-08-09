6aa4dd936d5e164b3fe88cf151cf8bdddc09665c
Adam Grare <adam@grare.com>
2023-08-09 09:13:11 -0400

    Refactor parsing payloads

--- lib/floe/workflow/payload_template.rb
+++ lib/floe/workflow/payload_template.rb
@@ -18,47 +18,57 @@ module Floe
 
       def parse_payload(value)
         case value
-        when Array
-          value.map { |val| parse_payload(val) }
-        when Hash
-          value.to_h do |key, val|
-            if key.end_with?(".$")
-              if value.key?(key.chomp(".$"))
-                raise Floe::InvalidWorkflowError, "both #{key} and #{key.chomp(".$")} present"
-              end
-
-              [key, parse_payload(val)]
-            else
-              [key, val]
-            end
-          end
-        when String
-          if value.start_with?("$")
-            Path.new(value)
-          else
-            value
-          end
+        when Array  then parse_payload_array(value)
+        when Hash   then parse_payload_hash(value)
+        when String then parse_payload_string(value)
         else
           value
         end
       end
 
+      def parse_payload_array(value)
+        value.map { |val| parse_payload(val) }
+      end
+
+      def parse_payload_hash(value)
+        value.to_h do |key, val|
+          if key.end_with?(".$")
+            if value.key?(key.chomp(".$"))
+              raise Floe::InvalidWorkflowError, "both #{key} and #{key.chomp(".$")} present"
+            end
+
+            [key, parse_payload(val)]
+          else
+            [key, val]
+          end
+        end
+      end
+
+      def parse_payload_string(value)
+        value.start_with?("$") ? Path.new(value) : value
+      end
+
       def interpolate_value(value, context, inputs)
         case value
-        when Array
-          value.map { |val| interpolate_value(val, context, inputs) }
-        when Hash
-          value.to_h do |key, val|
-            if key.end_with?(".$")
-              [key.chomp(".$"), interpolate_value(val, context, inputs)]
-            else
-              [key, val]
-            end
-          end
-        when Path
-          value.value(context, inputs)
+        when Array then interpolate_value_array(value, context, inputs)
+        when Hash  then interpolate_value_hash(value, context, inputs)
+        when Path  then value.value(context, inputs)
         else
           value
+        end
+      end
+
+      def interpolate_value_array(value, context, inputs)
+        value.map { |val| interpolate_value(val, context, inputs) }
+      end
+
+      def interpolate_value_hash(value, context, inputs)
+        value.to_h do |key, val|
+          if key.end_with?(".$")
+            [key.chomp(".$"), interpolate_value(val, context, inputs)]
+          else
+            [key, val]
+          end
         end
       end
     end
