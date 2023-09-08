85830da09e1b8d68973a56cb1fe3e7beea422709
Adam Grare <adam@grare.com>
2023-09-08 11:16:06 -0400

    Fix docker_spec runner_options

--- spec/workflow/runner/docker_spec.rb
+++ spec/workflow/runner/docker_spec.rb
@@ -3,9 +3,45 @@ RSpec.describe Floe::Workflow::Runner::Docker do
 
   let(:subject)        { described_class.new(runner_options) }
   let(:runner_options) { {} }
+  let(:container_id)   { SecureRandom.hex }
 
-  let(:subject)      { described_class.new }
-  let(:container_id) { SecureRandom.hex }
+  describe "#run!" do
+    it "raises an exception without a resource" do
+      expect { subject.run!(nil) }.to raise_error(ArgumentError, "Invalid resource")
+    end
+
+    it "raises an exception for an invalid resource uri" do
+      expect { subject.run!("arn:abcd:efgh") }.to raise_error(ArgumentError, "Invalid resource")
+    end
+
+    it "calls docker run with the image name" do
+      stub_good_run!("docker", :params => ["run", :rm, "hello-world:latest"])
+
+      subject.run!("docker://hello-world:latest")
+    end
+
+    it "passes environment variables to docker run" do
+      stub_good_run!("docker", :params => ["run", :rm, [:e, "FOO=BAR"], "hello-world:latest"])
+
+      subject.run!("docker://hello-world:latest", {"FOO" => "BAR"})
+    end
+
+    it "passes a secrets volume to docker run" do
+      stub_good_run!("docker", :params => ["run", :rm, [:e, "FOO=BAR"], [:e, "_CREDENTIALS=/run/secrets"], [:v, a_string_including(":/run/secrets")], "hello-world:latest"])
+
+      subject.run!("docker://hello-world:latest", {"FOO" => "BAR"}, {"luggage_password" => "12345"})
+    end
+
+    context "with network=host" do
+      let(:runner_options) { {"network" => "host"} }
+
+      it "calls docker run with --net host" do
+        stub_good_run!("docker", :params => ["run", :rm, [:net, "host"], "hello-world:latest"])
+
+        subject.run!("docker://hello-world:latest")
+      end
+    end
+  end
 
   describe "#run_async!" do
     it "raises an exception without a resource" do
@@ -32,6 +68,16 @@ RSpec.describe Floe::Workflow::Runner::Docker do
       stub_good_run!("docker", :params => ["run", :detach, [:e, "FOO=BAR"], [:e, "_CREDENTIALS=/run/secrets"], [:v, a_string_including(":/run/secrets")], "hello-world:latest"], :output => container_id)
 
       subject.run_async!("docker://hello-world:latest", {"FOO" => "BAR"}, {"luggage_password" => "12345"})
+    end
+
+    context "with network=host" do
+      let(:runner_options) { {"network" => "host"} }
+
+      it "calls docker run with --net host" do
+        stub_good_run!("docker", :params => ["run", :detach, [:net, "host"], "hello-world:latest"])
+
+        subject.run_async!("docker://hello-world:latest")
+      end
     end
   end
 
@@ -89,16 +135,6 @@ RSpec.describe Floe::Workflow::Runner::Docker do
       stub_bad_run!("docker", :params => ["rm", container_id])
       expect(secrets_file).to receive(:close!)
       subject.cleanup(container_id, secrets_file)
-    end
-
-    context "with network=host" do
-      let(:runner_options) { {"network" => "host"} }
-
-      it "calls docker run with --net host" do
-        stub_good_run!("docker", :params => ["run", :rm, "hello-world:latest"])
-
-        subject.run!("docker://hello-world:latest")
-      end
     end
   end
 end
