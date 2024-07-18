b130a131f14b4d52851843ce29ec7c36cba98c3f
Keenan Brock <keenan@thebrocks.net>
2024-07-18 19:20:15 -0400

    Add spec/supports
    I tend to want to require "byebug"
    I put this into the supports directory (and don't commit)
    
    This allows this and other local enhancement behavior
    
    fixup! Add support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ if ENV['CI']
   SimpleCov.start
 end
 
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
